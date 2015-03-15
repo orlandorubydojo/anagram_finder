91a2af9882dfe91f8f6240eaffe8487771cfafbc
Nestor Castillo <nestor.castillo@izea.com>
2015-03-15 16:27:34 -0400

    Setting files

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.1.5'
+
+gem 'rake', '~> 10.4.2'
+gem 'minitest', '~> 5.5.1'
+gem 'minitest-reporters', '~> 1.0.11'
+gem 'pry-byebug'
---
+++ test/anagram_finder_test.rb
@@ -0,0 +1,41 @@
+require './test_helper'
+require './lib/anagram_finder'
+
+class AnagramFinderTest < Minitest::Test
+
+  def test_instance_of_anagram_finder
+    skip
+  end
+
+  def test_anagram_finder_has_a_word
+    skip
+  end
+
+  def test_anagram_finder_has_a_dictionary
+    skip
+  end
+
+  def test_raises_error_if_no_word_is_given
+    skip
+  end
+
+  def test_detects_a_single_anagram
+    skip
+  end
+
+  def test_detects_multiple_anagrams
+     skip
+  end
+
+  def test_anagram_words_are_case_insensitive
+    skip
+  end
+
+  def test_display_message_if_no_anagrams_are_detected
+    skip
+  end
+
+  def test_indentical_words_are_not_anagrams
+    skip
+  end
+end
